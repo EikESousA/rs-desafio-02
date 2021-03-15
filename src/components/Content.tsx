13249bf41ed5043036bea5b383ae6a081585d511
EikESousA <eike.sousa@hotmail.com>
2021-03-15 19:35:40 -0300

    ok

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
-export function Content() {
-  // Complete aqui
+import { GenreResponseProps, MovieProps } from '../App';
+
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+interface IContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export default function Content({ selectedGenre, movies }:IContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
